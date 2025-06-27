05e6b434682016d1ce0670b1db1cec035931bc1f
bigbear <bigbear>
2025-06-27 22:06:09 +0800

    Initial commit

---
+++ packages/lib/dist/index.mjs
@@ -0,0 +1,10 @@
+function t(n, r) {
+  return n + r;
+}
+function u(n, r) {
+  return n - r;
+}
+export {
+  t as add,
+  u as minus
+};
---
+++ packages/lib/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib'
+
+export { minus } from './lib'
